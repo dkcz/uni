40ae4c3cb3018fdcc9338034bbc7e074e13b4fb0
dkcz <dk@keler.co>
2016-09-02 19:45:16 +0200

    v

--- clockbar.py
+++ clockbar.py
@@ -1,16 +1,16 @@
 import time
-import unicornhat as uh
+#import unicornhat as uh
 
-uh.set_layout(uh.HAT)
-uh.rotation(0)
-uh.brightness(1.0)
+#uh.set_layout(uh.HAT)
+#uh.rotation(0)
+#uh.brightness(1.0)
 
 
-def clr():
-    for x in range(8):
-        for y in range(8):
-            uh.set_pixel(x, y, 0, 0, 0)
-    uh.show()
+ def clr():
+     for x in range(8):
+         for y in range(8):
+             uh.set_pixel(x, y, 0, 0, 0)
+     uh.show()
 
 
 while True:
@@ -18,9 +18,9 @@ while True:
     # print (tm)
     line = 0
     posx = 0
-    if tm < 8:
+    if tm < 9:
         line = 0
-    elif 8 <= tm <= 16:
+    elif 9 <= tm <= 16:
         line = 1
     elif 17 <= tm <= 24:
         line = 2
@@ -32,16 +32,16 @@ while True:
         line = 5
     elif 49 <= tm <= 56:
         line = 6
-    elif 57 <= tm <= 60:
+    elif 57 <= tm <= 59:
         line = 7
 
     posx = tm - 8 * line - 1
     if posx == -1:
-        posx = 0
-    if tm == 0:
         clr()
+    # if tm == 0:
+    #     clr()
 
-    # print str(posx) + " , " + str(line) + " , " + str(tm)
+    #print str(posx) + " , " + str(line) + " , " + str(tm)
     uh.set_pixel(posx, line, 255, 255, 255)
     uh.show()
     time.sleep(1)
